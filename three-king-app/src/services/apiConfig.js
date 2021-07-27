a16ca445c9e2b455c4df734d77e0a7d6908a4f39
isaakamerigo <landen_g@yahoo.com>
2021-07-27 11:12:50 -0400

    created feature branch. created jsx file for components & screens. created boiler plate for app.js

--- three-king-app/src/services/apiConfig.js
+++ three-king-app/src/services/apiConfig.js
@@ -1,20 +1,9 @@
-import axios from 'axios'
+import axios from 'axios';
 
-let apiUrl
+const () {
+  return(
 
-const apiUrls = {
-    production: 'https://lg-kings.herokuapp.com/api/books/',
-    development: 'http://localhost:3000/api/books'
+  )
 }
 
-if (window.location.hostname === 'localhost') {
-    apiUrl = apiUrls.development
-} else {
-    apiUrl = apiUrls.production
-}
-
-const api = axios.create({
-    baseURL: apiUrl
-})
-
-export default api
+export default api;
