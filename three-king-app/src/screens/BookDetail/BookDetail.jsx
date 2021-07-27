d19975763007e3a348e05a309d82d25b40900ae9
isaakamerigo <landen_g@yahoo.com>
2021-07-27 15:10:50 -0400

    added export getBook to service/books.js then imported it to Bookdetail along with useState, useEffect & useParams

---
+++ three-king-app/src/screens/BookDetail/BookDetail.jsx
@@ -0,0 +1,13 @@
+import './BookDetail.css'
+import Nav from '../../components/Nav/Nav'
+// {}: importing a function
+import { getBook } from '../../services/books'
+import { useState, useEffect } from 'react'
+
+const BookDetail = () => {
+  return(
+
+    )
+}
+
+export default BookDetail;
